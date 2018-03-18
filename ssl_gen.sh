36573c3c3c73e1a1e981106acfc68e32da0f4b91
Felipe Zuleta <>
2018-03-18 11:29:23 -0500

    uses official certbox image

--- ssl_gen.sh
+++ ssl_gen.sh
@@ -6,7 +6,7 @@ DOMAIN=domain.com
 docker run -it --rm \
     -v $PWD/certs:/etc/letsencrypt \
     -v $PWD/certs-data:/data/letsencrypt \
-    deliverous/certbot \
+    certbot/certbot \
     certonly \
     --webroot --webroot-path=/data/letsencrypt \
     -d $DOMAIN
