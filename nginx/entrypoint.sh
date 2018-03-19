021d615da983978569651ceabdb3c9bccde95883
Felipe Zuleta <>
2018-03-19 04:12:34 -0500

    checks for the ssl cert folder instead of a random file

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -e "/done.exists" ]; then
+if [ -d "/etc/letsencrypt/live" ]; then
     rm /etc/nginx/conf.d/default.conf
     cp /default.conf /etc/nginx/conf.d/default.conf
     echo "Starting with both port 80 and 443"
@@ -8,7 +8,6 @@ else
     # use the por 80 only of the conf
     rm /etc/nginx/conf.d/default.conf
     cp /default_pre.conf /etc/nginx/conf.d/default.conf
-    touch "/done.exists"
     echo "Starting with port 80 only"
 fi
 
